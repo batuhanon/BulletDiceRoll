36fe8ead49ad9dec58fd1ecae0b0ff15d7719493
batuhanon <71566972+batuhanon@users.noreply.github.com>
2022-08-28 23:22:24 +0300

    Add files via upload

---
+++ PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    
+    public int playerHealth = 100;
+    //public GameObject deathEffect;
+
+    public void TakeDamage(int damage)
+    {
+        playerHealth -= damage;
+        if(playerHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        //Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ RollTheDice.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollTheDice : MonoBehaviour
+{
+    public PlayerHealth scriptHealth;
+    public BulletScript scriptBullet;
+    public PlayerMover scriptMover;
+
+    public GameObject bulletForDamage;
+
+    public float RadNum = 0f;
+
+    public void Start()
+    {
+        scriptBullet = bulletForDamage.GetComponent<BulletScript>();
+        PlayerHealth playerHealth = this.GetComponent<PlayerHealth>();
+    }
+
+    public void Update()
+    {
+        RadNum = Random.Range(1, 7);        
+        if (Input.GetKeyDown("r"))
+        {
+            if(RadNum == 6)
+            {
+                scriptHealth.playerHealth *= 10;
+            }
+            else if (RadNum == 5)
+            {
+                scriptBullet.damage *= 10;
+            }
+            else if (RadNum == 4)
+            {
+                scriptBullet.damage *= 2;
+            }
+            else if (RadNum == 3)
+            {
+                scriptBullet.damage = 25;
+            }
+            else if (RadNum == 2)
+            {
+                scriptMover.runSpeed = 2;
+            }
+            else if (RadNum == 1)
+            {
+                scriptHealth.Die();
+            }
+
+        }
+        
+        
+    }
+   
+}
